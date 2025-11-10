81e199b87a5a828306c2b946d7078fa209bdf934
vicmmoraM <vicmmora@espol.edu.ec>
2025-11-10 14:09:42 -0500

    feat: add new resources

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { Grid } from '@mui/material';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Grid>
+
+         {/* Encabezado */}
+         <Grid>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid>Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid>Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid>Elemento: Información adicional</Grid>
+
+      </Grid>
+  )
+}
+
+export default App
