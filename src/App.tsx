12baf52708285d63f8777175563003d380b28c4d
vicmmoraM <vicmmora@espol.edu.ec>
2025-11-12 13:47:49 -0500

    cambios xs y md

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,30 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Grid>
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
 
-         {/* Encabezado */}
-         <Grid>Elemento: Encabezado</Grid>
+      {/* Encabezado */}
+      <Grid size = {{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
 
-         {/* Alertas */}
-         <Grid>Elemento: Alertas</Grid>
+      {/* Alertas */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
 
-         {/* Selector */}
-         <Grid>Elemento: Selector</Grid>
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
 
-         {/* Indicadores */}
-         <Grid>Elemento: Indicadores</Grid>
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
-         {/* Gráfico */}
-         <Grid>Elemento: Gráfico</Grid>
+      {/* Gráfico */}
+      <Grid sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
 
-         {/* Tabla */}
-         <Grid>Elemento: Tabla</Grid>
+      {/* Tabla */}
+      <Grid sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
 
-         {/* Información adicional */}
-         <Grid>Elemento: Información adicional</Grid>
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
 
-      </Grid>
+    </Grid>
   )
 }
 
